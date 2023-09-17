127354c6f5e3a82a7dfa410dde40f92a58cbfac2
Akito-Fujihara <1998922akito@gmail.com>
2023-09-17 19:51:22 +0900

    sgの追加

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -21,3 +21,23 @@ resource "aws_security_group" "alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "eks_node" {
+  name   = "akito-eks-node"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port       = 30000
+    to_port         = 30000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+    self            = true
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
