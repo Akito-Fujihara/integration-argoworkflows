90d6cbb17dae397637a2fed2f00d9211e3a5bdcf
Akito-Fujihara <1998922akito@gmail.com>
2023-10-02 23:04:00 +0900

    rds sgの作成

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -21,3 +21,21 @@ resource "aws_security_group" "alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "rds" {
+  name   = "${var.name}-rds"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [module.eks.node_security_group_id]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
